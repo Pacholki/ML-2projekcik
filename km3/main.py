41b57ac4cbce04b7e854407f2ddc6a3f6ea92e5e
Kornel <kornel.tlaczala@gmail.com>
2024-06-02 12:22:56 +0200

    reading parameter values and saving them

--- km3/main.py
+++ km3/main.py
@@ -18,11 +18,11 @@ class App(ctk.CTk):
 
         self.title("Model Viewer")
         self.geometry("1200x800")
+        self.minsize(1200, 800)
 
         self.data_file_path = "data/dataset.csv"
         self.clusterator = clustering.Clusterator(self.data_file_path)
         self.columns = self.clusterator.get_columns()
-        print(self.columns)
         self.models = self.read_models()
         self.model=None
 
@@ -40,16 +40,20 @@ class App(ctk.CTk):
         self.save_frame = SaveFrame(self)
         self.save_frame.grid(row=2, column=0, columnspan=2, padx=20, pady=20, sticky="nsew")
         
+        
     def run(self):
         self.mainloop()
 
     def read_models(self):
         try:
-            with open("data/default_models.json", "r") as file:
-                return json.load(file)
-        except FileNotFoundError:
             with open("data/models.json", "r") as file:
                 return json.load(file)
+        except FileNotFoundError:
+            try:
+                with open("data/default_models.json", "r") as file:
+                    return json.load(file)
+            except FileNotFoundError:
+                raise FileNotFoundError("No models file found")
     
     def models_revert_to_default(self):
         try:
@@ -57,6 +61,10 @@ class App(ctk.CTk):
             print(f"Modesl reverted to default")
         except FileNotFoundError:
             print("No custom models found")
+    
+    def save_models(self):
+        with open("data/models.json", "w") as file:
+            json.dump(self.models, file, indent=4)
         
 
 class ModelChoiceFrame(ctk.CTkFrame):
@@ -65,24 +73,45 @@ class ModelChoiceFrame(ctk.CTkFrame):
         super().__init__(master)
         self.master = master
         self.model_names = list(master.models.keys())
-        self.param_frame = ctk.CTkFrame(master=self)
         self.create_widgets()
-        self.param_frame.pack(pady=20, padx=20, fill="both", expand=True)
 
 
     def create_widgets(self):
         self.model_choice = ctk.CTkComboBox(master=self, values=self.model_names, command=self.on_model_choice)
-        self.model_choice.pack()
+        self.model_choice.configure(width=250, height=30)
+        self.model_choice.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
+
+        self.param_frame = ctk.CTkFrame(master=self)
+        self.column_frame = ctk.CTkFrame(master=self)
+        self.value_frame = ctk.CTkFrame(master=self)
+        self.param_frame.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
+        self.column_frame.grid(row=1, column=1, padx=5, pady=5, sticky="nsew")
+        self.value_frame.grid(row=1, column=2, padx=5, pady=5, sticky="nsew")
+
+        self.calculate_button = ctk.CTkButton(master=self, text="Calculate", command=self.calculate)
+        self.calculate_button.grid(row=2, column=0, columnspan=3, padx=5, pady=5, sticky="nsew")
+
+        self.on_model_choice(self.model_choice.get())
 
     def on_model_choice(self, model_name):
+        print("siema")
         for widget in self.param_frame.winfo_children():
             widget.destroy()
-        params = {k: v for k, v in self.master.models[model_name]["params"].items() if v["active"]}
-        for param, details in params.items():
+        self.params = {k: v for k, v in self.master.models[model_name]["params"].items() if v["active"]}
+        self.entries = {}
+        for i, (param, details) in enumerate(self.params.items()):
             label = ctk.CTkLabel(master=self.param_frame, text=param)
-            field = ctk.CTkEntry(master=self.param_frame, placeholder_text=details["value"])
-            label.pack()
-            field.pack()
+            entry = ctk.CTkEntry(master=self.param_frame, placeholder_text=details["value"])
+            label.grid(row=i, column=0, padx=5, pady=5, sticky="w")
+            entry.grid(row=i, column=1, padx=5, pady=5, sticky="ew")
+            self.entries[param] = entry
+
+    def calculate(self):
+        for param, entry in self.entries.items():
+            value = entry.get()
+            if value:
+                self.master.models[self.model_choice.get()]["params"][param]["value"] = value
+
 
 class StatsFrame(ctk.CTkFrame):
 
@@ -116,3 +145,4 @@ class GraphFrame(ctk.CTkFrame):
 if __name__ == "__main__":
     app = App()
     app.run()
+    app.save_models()
